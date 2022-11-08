b19422d8c39e1e102a814e8095d031c89552d31f
lei <mia.wang90@gmail.com>
2022-11-08 18:01:45 +0900

    Add detail component

---
+++ components/meetups/MeetupDetail.js
@@ -0,0 +1,12 @@
+import classes from "./MeetupDetail.module.css";
+
+export default function MeetupDetails(props) {
+  return (
+    <section className={classes.detail}>
+      <img src={props.image} alt={props.title} />
+      <h1>{props.title}</h1>
+      <p>{props.description}</p>
+      <address>{props.address}</address>
+    </section>
+  );
+}
---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,12 @@
+import MeetupDetails from "../../components/meetups/MeetupDetail";
+
+export default function MeetupDetailsPage() {
+  return (
+    <MeetupDetails
+      title="A First Meetup"
+      image="https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg"
+      address="Some address 5, 1245 Some City"
+      description="This is a first description"
+    />
+  );
+}
