64eada430f24e39c26c8cc7c55cbaf96dad20bed
lei <mia.wang90@gmail.com>
2022-11-11 13:36:21 +0900

    Add mongoDB

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,12 +1,51 @@
-import MeetupDetails from "../../components/meetups/MeetupDetail";
+import MeetupDetail from "../../components/meetups/MeetupDetail";
 
-export default function MeetupDetailsPage() {
+export default function MeetupDetailPage() {
   return (
-    <MeetupDetails
+    <MeetupDetail
       title="A First Meetup"
       image="https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg"
       address="Some address 5, 1245 Some City"
       description="This is a first description"
     />
   );
+}
+
+export async function getStaticPaths() {
+  return {
+    fallback: false,
+    paths: [
+      {
+        params: {
+          meetupId: "m1",
+        },
+      },
+      {
+        params: {
+          meetupId: "m2",
+        },
+      },
+    ],
+  };
+}
+
+// code execute when npm run build, not on server-side or client-side
+export async function getStaticProps(context) {
+  // fetch data for a single meetup
+
+  const meetupId = context.params.meetupId;
+
+  return {
+    props: {
+      meetupData: {
+        id: meetupId,
+        title: "A First Meetup",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg",
+        address: "Some address 5, 1245 Some City",
+        description: "This is a first description",
+      },
+    },
+    revalidate: 1,
+  };
 }
--- pages/index.js
+++ pages/index.js
@@ -19,6 +19,32 @@ const DUMMY_MEETUPS = [
   },
 ];
 
-export default function HomePage() {
-  return <MeetupList meetups={DUMMY_MEETUPS} />;
+export default function HomePage(props) {
+  return <MeetupList meetups={props.meetups} />;
 }
+
+// code execute when npm run build, not on server-side or client-side
+export async function getStaticProps() {
+  // fetch data from an API
+
+  return {
+    props: {
+      meetups: DUMMY_MEETUPS,
+    },
+    revalidate: 1,
+  };
+}
+
+// Never use SSG, unless content changes multiple times every second
+// export async function getServerSideProps(context) {
+//   const req = context.req;
+//   const res = context.res;
+
+//   // fetch data from an API
+//   // code only run on the server
+//   return {
+//     props: {
+//       meetups: DUMMY_MEETUPS,
+//     },
+//   };
+// }
